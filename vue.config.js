0dbf5b42a32322484616b969078cf4e85a2cb09a
x-cold <lxstart@outlook.com>
2020-05-03 09:50:42 +0800

    chore: support build and ci

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const path = require('path')
 module.exports = {
-  publicPath: './',
   // lintOnSave: true,
   chainWebpack: (config) => {
     config.resolve.alias
       .set('@', path.join(__dirname, 'src'))
       .set('@assets', path.join(__dirname, 'src/assets'))
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-mall-admin/'
+    : '/'
 }
