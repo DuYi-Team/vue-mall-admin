592bb408c983e795f9eb295798a918ec67295138
董美琪 <1125479840@qq.com>
2020-05-18 16:46:59 +0800

    去掉国际化

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <a-locale-provider :locale="zhCN">
     <div id="app">
       <router-view></router-view>
     </div>
-  </a-locale-provider>
 </template>
 
 <script>
-import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
-
 export default {
   name: 'app',
   data () {
     return {
-      zhCN
     }
   }
 
